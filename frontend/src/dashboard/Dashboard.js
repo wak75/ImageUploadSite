c7249c0edda4102788f2330dbe2c4a7e9ccf80f6
rithik609609 <63301333+rithik609609@users.noreply.github.com>
2024-11-28 23:12:58 +0530

    Added Card View

--- frontend/src/dashboard/Dashboard.js
+++ frontend/src/dashboard/Dashboard.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React , {useState}from 'react'
 import './Dashboard.css' 
+import FormControlLabel from '@mui/material/FormControlLabel'
+import Switch from '@mui/material/Switch'
+
+const bucket = [
+  'assets/img/img1.jpeg',
+  'assets/img/img2.jpeg',
+  'assets/img/img3.jpeg',
+];
 
 export default function Dashboard() {
+  const [images, setImages] = useState(bucket);
+  
+  const handleDelete = (index) => {
+    const updatedImages = images.filter((_, i) => i !== index);
+    // const updatedToggleStates = toggleStates.filter((_, i) => i !== index);
+    setImages(updatedImages);
+    // setToggleStates(updatedToggleStates);
+  };
+
   return (
     <div>
       <header>
@@ -22,6 +39,17 @@ export default function Dashboard() {
       </label>
     </div>
   </div>
+  <div className="card-container">
+        {images.map((image, index) => (
+          <div className="card" key={index}>
+            <img src={image} alt={`Image ${index + 1}`} className="card-image" />
+            <div className="card-actions">
+            <FormControlLabel  label="Private" control={<Switch defaultChecked />} labelPlacement="start" />
+              <button onClick={() => handleDelete(index)} >Delete</button>
+            </div>
+          </div>
+        ))}
+      </div>
     </div>
   )
 }
