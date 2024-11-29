560379093147d1bdb9bdc18c80933f752196e9c6
Rithik Prakash <rithik.prakash@sap.com>
2024-11-29 12:35:08 +0530

    Added Upload Button

--- frontend/src/dashboard/Dashboard.js
+++ frontend/src/dashboard/Dashboard.js
@@ -1,17 +1,18 @@
-import React , {useState}from 'react'
-import './Dashboard.css' 
-import FormControlLabel from '@mui/material/FormControlLabel'
-import Switch from '@mui/material/Switch'
+import React, { useState } from "react";
+import "./Dashboard.css";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import Switch from "@mui/material/Switch";
+import { BsImage } from "react-icons/bs";
 
 const bucket = [
-  'assets/img/img1.jpeg',
-  'assets/img/img2.jpeg',
-  'assets/img/img3.jpeg',
+  "assets/img/img1.jpeg",
+  "assets/img/img2.jpeg",
+  "assets/img/img3.jpeg",
 ];
 
 export default function Dashboard() {
   const [images, setImages] = useState(bucket);
-  
+
   const handleDelete = (index) => {
     const updatedImages = images.filter((_, i) => i !== index);
     // const updatedToggleStates = toggleStates.filter((_, i) => i !== index);
@@ -22,34 +23,44 @@ export default function Dashboard() {
   return (
     <div>
       <header>
-    <nav className="navbar">
-      <div className="navdiv">
-        <div><a href="\">ImageUpload</a></div>
-        <div className="divimg">
-          <img src="/assets/img/user.png" alt=""/>
-        </div>
-      </div>
-    </nav>
-  </header>
-  <div className="container">
-    <div className="upload">
-      <label className="upload-button">
-        Upload File
-        <input type="file" class="upload-input"/>
-      </label>
-    </div>
-  </div>
-  <div className="card-container">
+        <nav className="navbar">
+          <div className="navdiv">
+            <div>
+              <a href="\">ImageUpload</a>
+            </div>
+            <div className="divimg">
+              <img src="/assets/img/user.png" alt="" />
+            </div>
+          </div>
+        </nav>
+      </header>
+      <div className="card-container">
         {images.map((image, index) => (
           <div className="card" key={index}>
-            <img src={image} alt={`Image ${index + 1}`} className="card-image" />
+            <img
+              src={image}
+              alt={`Image ${index + 1}`}
+              className="card-image"
+            />
             <div className="card-actions">
-            <FormControlLabel  label="Private" control={<Switch defaultChecked />} labelPlacement="start" />
-              <button onClick={() => handleDelete(index)} >Delete</button>
+              <FormControlLabel
+                label="Private"
+                control={<Switch defaultChecked />}
+                labelPlacement="start"
+              />
+              <button onClick={() => handleDelete(index)}>Delete</button>
             </div>
           </div>
         ))}
+        <div className="uploadcard">
+        <label htmlFor="drop-file" className="uploadLabel">
+          <BsImage className="uploadIcon" />
+          <h2>Add Images</h2>
+          <input type="file" name="gallery-img" id="drop-file" hidden />
+        </label>
       </div>
+      </div>
+
     </div>
-  )
+  );
 }
